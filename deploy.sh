68070e4d47a773f24222dfd95c6429c0173034dc
Ensar Džafo <90572926+EnsarDzaf0@users.noreply.github.com>
2024-05-28 22:31:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,7 @@ fi
 if ! command -v node &> /dev/null
 then
     echo "Installing Node.js..."
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
     sudo apt-get install -y nodejs
 fi
 
